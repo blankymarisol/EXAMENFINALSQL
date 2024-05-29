d3a719b46066b6e82ca69569f7477d6dcb3c192d
blanksss <marrophph@gmail.com>
2024-05-28 19:57:19 -0600

    Agregar archivos de proyecto.

---
+++ EXAMENFINALSQL/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace EXAMENFINALSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxnombre = new System.Windows.Forms.TextBox();
+            this.textBoxalias = new System.Windows.Forms.TextBox();
+            this.textBoxestado = new System.Windows.Forms.TextBox();
+            this.textBoxemail = new System.Windows.Forms.TextBox();
+            this.textBoxusername = new System.Windows.Forms.TextBox();
+            this.textBoxduracion = new System.Windows.Forms.TextBox();
+            this.buttonprobar = new System.Windows.Forms.Button();
+            this.buttonver = new System.Windows.Forms.Button();
+            this.buttonactualizar = new System.Windows.Forms.Button();
+            this.buttoneliminar = new System.Windows.Forms.Button();
+            this.buttoncrear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonbuscar = new System.Windows.Forms.Button();
+            this.buttonsalir = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Coral;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(674, 175);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SandyBrown;
+            this.label1.Location = new System.Drawing.Point(36, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ESTADO DE PARTIDA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 625);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SandyBrown;
+            this.label3.Location = new System.Drawing.Point(36, 636);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "EMAIL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SandyBrown;
+            this.label4.Location = new System.Drawing.Point(36, 585);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "USERNAME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.SandyBrown;
+            this.label5.Location = new System.Drawing.Point(36, 531);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DURACION DE LA PARTIDA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.SandyBrown;
+            this.label6.Location = new System.Drawing.Point(36, 427);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ALIAS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.SandyBrown;
+            this.label7.Location = new System.Drawing.Point(36, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "NOMBRE";
+            // 
+            // textBoxnombre
+            // 
+            this.textBoxnombre.Location = new System.Drawing.Point(129, 369);
+            this.textBoxnombre.Name = "textBoxnombre";
+            this.textBoxnombre.Size = new System.Drawing.Size(124, 22);
+            this.textBoxnombre.TabIndex = 8;
+            // 
+            // textBoxalias
+            // 
+            this.textBoxalias.Location = new System.Drawing.Point(129, 424);
+            this.textBoxalias.Name = "textBoxalias";
+            this.textBoxalias.Size = new System.Drawing.Size(124, 22);
+            this.textBoxalias.TabIndex = 9;
+            // 
+            // textBoxestado
+            // 
+            this.textBoxestado.Location = new System.Drawing.Point(234, 476);
+            this.textBoxestado.Name = "textBoxestado";
+            this.textBoxestado.Size = new System.Drawing.Size(124, 22);
+            this.textBoxestado.TabIndex = 10;
+            // 
+            // textBoxemail
+            // 
+            this.textBoxemail.Location = new System.Drawing.Point(117, 636);
+            this.textBoxemail.Name = "textBoxemail";
+            this.textBoxemail.Size = new System.Drawing.Size(124, 22);
+            this.textBoxemail.TabIndex = 11;
+            // 
+            // textBoxusername
+            // 
+            this.textBoxusername.Location = new System.Drawing.Point(168, 582);
+            this.textBoxusername.Name = "textBoxusername";
+            this.textBoxusername.Size = new System.Drawing.Size(124, 22);
+            this.textBoxusername.TabIndex = 12;
+            // 
+            // textBoxduracion
+            // 
+            this.textBoxduracion.Location = new System.Drawing.Point(267, 531);
+            this.textBoxduracion.Name = "textBoxduracion";
+            this.textBoxduracion.Size = new System.Drawing.Size(124, 22);
+            this.textBoxduracion.TabIndex = 13;
+            // 
+            // buttonprobar
+            // 
+            this.buttonprobar.BackColor = System.Drawing.Color.Tan;
+            this.buttonprobar.Location = new System.Drawing.Point(30, 265);
+            this.buttonprobar.Name = "buttonprobar";
+            this.buttonprobar.Size = new System.Drawing.Size(167, 72);
+            this.buttonprobar.TabIndex = 14;
+            this.buttonprobar.Text = "PROBAR CONEXION";
+            this.buttonprobar.UseVisualStyleBackColor = false;
+            // 
+            // buttonver
+            // 
+            this.buttonver.Location = new System.Drawing.Point(263, 265);
+            this.buttonver.Name = "buttonver";
+            this.buttonver.Size = new System.Drawing.Size(128, 62);
+            this.buttonver.TabIndex = 15;
+            this.buttonver.Text = "VER DATOS";
+            this.buttonver.UseVisualStyleBackColor = true;
+            // 
+            // buttonactualizar
+            // 
+            this.buttonactualizar.Location = new System.Drawing.Point(506, 366);
+            this.buttonactualizar.Name = "buttonactualizar";
+            this.buttonactualizar.Size = new System.Drawing.Size(128, 62);
+            this.buttonactualizar.TabIndex = 16;
+            this.buttonactualizar.Text = "ACTUALIZAR";
+            this.buttonactualizar.UseVisualStyleBackColor = true;
+            // 
+            // buttoneliminar
+            // 
+            this.buttoneliminar.Location = new System.Drawing.Point(506, 438);
+            this.buttoneliminar.Name = "buttoneliminar";
+            this.buttoneliminar.Size = new System.Drawing.Size(128, 62);
+            this.buttoneliminar.TabIndex = 17;
+            this.buttoneliminar.Text = "ELIMINAR REGISTRO";
+            this.buttoneliminar.UseVisualStyleBackColor = true;
+            // 
+            // buttoncrear
+            // 
+            this.buttoncrear.Location = new System.Drawing.Point(506, 524);
+            this.buttoncrear.Name = "buttoncrear";
+            this.buttoncrear.Size = new System.Drawing.Size(128, 62);
+            this.buttoncrear.TabIndex = 18;
+            this.buttoncrear.Text = "CREAR NUEVO REGISTRO";
+            this.buttoncrear.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 62);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "LIMPIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.SandyBrown;
+            this.label8.Location = new System.Drawing.Point(292, 369);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "No. Partida";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(387, 366);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 22);
+            this.textBox1.TabIndex = 21;
+            // 
+            // buttonbuscar
+            // 
+            this.buttonbuscar.Location = new System.Drawing.Point(506, 609);
+            this.buttonbuscar.Name = "buttonbuscar";
+            this.buttonbuscar.Size = new System.Drawing.Size(128, 62);
+            this.buttonbuscar.TabIndex = 22;
+            this.buttonbuscar.Text = "BUSCAR POR No. PARTIDA";
+            this.buttonbuscar.UseVisualStyleBackColor = true;
+            // 
+            // buttonsalir
+            // 
+            this.buttonsalir.Location = new System.Drawing.Point(637, 694);
+            this.buttonsalir.Name = "buttonsalir";
+            this.buttonsalir.Size = new System.Drawing.Size(73, 31);
+            this.buttonsalir.TabIndex = 23;
+            this.buttonsalir.Text = "SALIR";
+            this.buttonsalir.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe Script", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(41, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(642, 36);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "BIENVENID@ A LOS DATOS DE ALEJOGAME!!!! <3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(722, 737);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.buttonsalir);
+            this.Controls.Add(this.buttonbuscar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttoncrear);
+            this.Controls.Add(this.buttoneliminar);
+            this.Controls.Add(this.buttonactualizar);
+            this.Controls.Add(this.buttonver);
+            this.Controls.Add(this.buttonprobar);
+            this.Controls.Add(this.textBoxduracion);
+            this.Controls.Add(this.textBoxusername);
+            this.Controls.Add(this.textBoxemail);
+            this.Controls.Add(this.textBoxestado);
+            this.Controls.Add(this.textBoxalias);
+            this.Controls.Add(this.textBoxnombre);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxnombre;
+        private System.Windows.Forms.TextBox textBoxalias;
+        private System.Windows.Forms.TextBox textBoxestado;
+        private System.Windows.Forms.TextBox textBoxemail;
+        private System.Windows.Forms.TextBox textBoxusername;
+        private System.Windows.Forms.TextBox textBoxduracion;
+        private System.Windows.Forms.Button buttonprobar;
+        private System.Windows.Forms.Button buttonver;
+        private System.Windows.Forms.Button buttonactualizar;
+        private System.Windows.Forms.Button buttoneliminar;
+        private System.Windows.Forms.Button buttoncrear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonbuscar;
+        private System.Windows.Forms.Button buttonsalir;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
